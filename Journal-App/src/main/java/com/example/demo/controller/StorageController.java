253fc6583e9aaffaae57f19b21e3a68dc429fe9d
aizirek19 <145474532+aizirek19@users.noreply.github.com>
2024-12-12 21:10:24 +0600

    Add files via upload

--- Journal-App/src/main/java/com/example/demo/controller/StorageController.java
+++ Journal-App/src/main/java/com/example/demo/controller/StorageController.java
@@ -10,6 +10,52 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
+//@RestController
+//@RequestMapping("/images")
+//public class StorageController {
+//
+//    @Autowired
+//    private StorageService storageService;
+//
+//    // Upload an image (POST request)
+//    @PostMapping("/upload")
+//    public ResponseEntity<String> uploadImage(@RequestParam("file") MultipartFile file) {
+//        try {
+//            String message = storageService.uploadImage(file);
+//            return ResponseEntity.ok(message);
+//        } catch (IOException e) {
+//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to upload image");
+//        }
+//    }
+//
+//    // Download an image (GET request)
+//    @GetMapping("/download/{fileName}")
+//    public ResponseEntity<byte[]> downloadImage(@PathVariable String fileName) {
+//        try {
+//            byte[] imageData = storageService.downloadImage(fileName);
+//            return ResponseEntity.ok()
+//                    .contentType(MediaType.parseMediaType("image/jpeg")) // Set content type based on your image type
+//                    .body(imageData);
+//        } catch (IOException e) {
+//            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+//        }
+//    }
+//
+//    // Delete an image (DELETE request)
+//    @DeleteMapping("/delete/{fileName}")
+//    public ResponseEntity<String> deleteImage(@PathVariable String fileName) {
+//        boolean isDeleted = storageService.deleteImage(fileName);
+//
+//        if (isDeleted) {
+//            return ResponseEntity.ok("Image deleted successfully: " + fileName);
+//        } else {
+//            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Image not found: " + fileName);
+//        }
+//    }
+//}
+
+
+
 @RestController
 @RequestMapping("/image")
 public class StorageController {
